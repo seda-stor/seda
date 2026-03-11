78b5a30e84790495c75d7017aad36576dd8acd25
seda-stor <aliali33218511@gmail.com>
2026-03-11 13:46:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,17 @@
-const cacheName = 'seda-store-v1';
+const cacheName = 'seda-cache-v1';
 const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/seda/',
+  '/seda/index.html',
+  '/seda/manifest.json',
+  '/seda/icon-192.png',
+  '/seda/icon-512.png'
 ];
 
-
-// تثبيت الـ Service Worker وتخزين الملفات
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// جلب البيانات من التخزين المؤقت عند انقطاع الشبكة
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
