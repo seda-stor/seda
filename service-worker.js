173f4e55375e7edd46e27bb9cc1f3fa43c134483
seda-stor <aliali33218511@gmail.com>
2026-03-11 21:01:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,10 @@
-const cacheName = 'seda-cache-v1';
-const assets = [
-  '/seda/',
-  '/seda/index.html',
-  '/seda/manifest.json',
-  '/seda/icon-192.png',
-  '/seda/icon-512.png'
-];
+const cacheName = 'seda-v1';
+const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
-
